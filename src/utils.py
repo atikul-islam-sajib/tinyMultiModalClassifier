62f13bff71a8057f8c877db38cbe172a4b1a8ec7
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 08:22:20 +0100

    device init is added in the utils file

--- src/utils.py
+++ src/utils.py
@@ -32,3 +32,12 @@ def weight_init(m):
         nn.init.kaiming_normal_(m.weight.data)
         if m.bias is not None:
             nn.init.constant_(m.bias.data, 0.0)
+
+
+def device_init(device: str = "cuda"):
+    if device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    elif device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
