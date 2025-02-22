b3ae44ab2ae0379f5235facf7d6f87ff79211af4
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 11:05:45 +0100

    prototype of transformer encoder block is added

--- src/transformer_encoder_block.py
+++ src/transformer_encoder_block.py
@@ -6,37 +6,39 @@ import torch.nn as nn
 
 sys.path.append("./src/")
 
-from transformer_encoder_block import TransformerEncoderBlock
+from multihead_attention_layer import MultiHeadAttentionLayer
 
 
-class TransformerEncoder(nn.Module):
+class TransformerEncoderBlock(nn.Module):
     def __init__(
         self,
         dimension: int = 256,
         nheads: int = 8,
-        num_encoder_layers: int = 8,
         dim_feedforward: int = 1024,
         dropout: float = 0.1,
     ):
-        super(TransformerEncoder, self).__init__()
+        super(TransformerEncoderBlock, self).__init__()
         self.dimension = dimension
         self.nheads = nheads
-        self.num_encoder_layers = num_encoder_layers
         self.dim_feedforward = dim_feedforward
 
-        self.layers = nn.Sequential(
-            *[
-                TransformerEncoderBlock(nheads=self.nheads, dimension=self.dimension)
-                for _ in range(self.num_encoder_layers)
-            ]
+        self.multihead_attention = MultiHeadAttentionLayer(
+            nheads=self.nheads, dimension=self.dimension
         )
 
     def forward(self, x: torch.Tensor):
         if isinstance(x, torch.Tensor):
-            pass
+            residual = x
+
+            x = self.multihead_attention(x)
+            return x
         else:
             raise ValueError("Input must be a torch.Tensor.")
 
 
 if __name__ == "__main__":
-    pass
+    transformer_encoder_block = TransformerEncoderBlock(
+        dimension=256,
+        nheads=8,
+    )
+    print(torch.randn((1, 64, 256)).size())
