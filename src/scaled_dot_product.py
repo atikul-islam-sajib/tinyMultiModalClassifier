731dcd877b70dddffafd083aa199c6950980fbf5
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 10:13:59 +0100

    scaled dot product is completed

--- src/scaled_dot_product.py
+++ src/scaled_dot_product.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 import torch
 import torch.nn as nn
 
@@ -12,6 +13,19 @@ def scaled_dot_product(query: torch.Tensor, key: torch.Tensor, values: torch.Ten
         and isinstance(key, torch.Tensor)
         and isinstance(values, torch.Tensor)
     ):
-        pass
+        logits = torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(query.size(-1))
+        attention_weights = torch.softmax(logits, dim=-1)
+        attention_output = torch.matmul(attention_weights, values)
+        return attention_output
+
     else:
         raise ValueError("All inputs must be torch tensors.".capitalize())
+
+
+if __name__ == "__main__":
+    attention = scaled_dot_product(
+        torch.randn((1, 8, 64, 32)),
+        torch.randn((1, 8, 64, 32)),
+        torch.randn((1, 8, 64, 32)),
+    )
+    print(attention.size())  # (1, 8, 64, 32)
