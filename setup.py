ba6d77292d05457f3a779c31bfe3e0e847ae6be8
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 08:11:18 +0100

    setup file is added in the code

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tinyMultiModalClassifier",
+    version="0.0.1",
+    description="A deep learning project for processing the image and textual data which is known as the Multimodal",
+    author="Atikul Islam Sajib",
+    author_email="atikulislamsajib137@gmail.com",
+    url="https://github.com/atikul-islam-sajib/tinyMultiModalClassifier",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="tinyMultiModalClassifier, deep-learning",
+    project_urls={
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/tinyMultiModalClassifier/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/tinyMultiModalClassifier",
+        "Source Code": "https://github.com/atikul-islam-sajib/tinyMultiModalClassifier",
+    },
+)
