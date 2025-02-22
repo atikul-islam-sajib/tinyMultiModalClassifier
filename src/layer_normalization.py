c250646a916c1303c936efb9016e20372985b2c6
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 11:37:28 +0100

    layer normalization is added

--- src/layer_normalization.py
+++ src/layer_normalization.py
@@ -10,13 +10,26 @@ sys.path.append("./src/")
 class LayerNormalization(nn.Module):
     def __init__(self, dimension: int = 256):
         super(LayerNormalization, self).__init__()
+        self.dimension = dimension
+
+        self.alpha = nn.Parameter(
+            data=torch.randn((1, 1, self.dimension)), requires_grad=True
+        )
+        self.beta = nn.Parameter(
+            data=torch.randn((1, 1, self.dimension)), requires_grad=True
+        )
 
     def forward(self, x: torch.Tensor):
         if isinstance(x, torch.Tensor):
-            pass
+            mean = torch.mean(x, dim=-1).unsqueeze(-1)
+            variance = torch.var(x, dim=-1).unsqueeze(-1)
+
+            return self.alpha * mean + self.beta * variance
+
         else:
             raise ValueError("Input must be a torch.Tensor.".capitalize())
 
 
 if __name__ == "__main__":
-    pass
+    layer_normalization = LayerNormalization(dimension=256)
+    print(layer_normalization(torch.randn((1, 64, 256))).size())
