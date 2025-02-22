a422229671c04bf50524e4fed6421ee6296b0eab
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 09:46:57 +0100

    QKV is added

--- src/transformer_encoder_block.py
+++ src/transformer_encoder_block.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import warnings
 import torch
 import torch.nn as nn
 
@@ -12,12 +13,30 @@ class TransformerEncoderBlock(nn.Module):
         self.nheads = nheads
         self.dimension = dimension
 
+        warnings.warn(
+            "Please ensure that the dimension is a multiple of the number of heads in the encoder block (e.g., 256 % 8 = 0). "
+            "This is a requirement for the Transformer Encoder Block to function properly. "
+            "If not, you might need to adjust the dimension or the number of heads."
+        )
+        assert (
+            dimension % self.nheads == 0
+        ), "Dimension mismatched with nheads and dimension".title()
+
+        self.QKV = nn.Linear(
+            in_features=self.dimension, out_features=3 * self.dimension, bias=False
+        )
+
     def forward(self, x: torch.Tensor):
         if isinstance(x, torch.Tensor):
-            pass
+            x = self.QKV(x)
+            return x
         else:
             raise ValueError("Input must be a torch tensor.".capitalize())
 
 
 if __name__ == "__main__":
-    pass
+    transformerEncoder = TransformerEncoderBlock(
+        nheads=8,
+        dimension=256,
+    )
+    print(transformerEncoder(torch.randn((1, 64, 256))).size())
