69691f5cdd052b17a6ebee35258c28a5063f63c8
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-22 11:44:32 +0100

    Transformer Encoder Block is added in the src

--- src/transformer_encoder_block.py
+++ src/transformer_encoder_block.py
@@ -7,6 +7,8 @@ import torch.nn as nn
 sys.path.append("./src/")
 
 from multihead_attention_layer import MultiHeadAttentionLayer
+from feed_forward_network import FeedForwardNeuralNetwork
+from layer_normalization import LayerNormalization
 
 
 class TransformerEncoderBlock(nn.Module):
@@ -25,12 +27,28 @@ class TransformerEncoderBlock(nn.Module):
         self.multihead_attention = MultiHeadAttentionLayer(
             nheads=self.nheads, dimension=self.dimension
         )
+        self.layer_normalization = LayerNormalization(dimension=self.dimension)
+        self.feed_forward_network = FeedForwardNeuralNetwork(
+            in_features=self.dimension, out_features=4 * self.dim_feedforward
+        )
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x: torch.Tensor):
         if isinstance(x, torch.Tensor):
             residual = x
 
             x = self.multihead_attention(x)
+            x = self.dropout(x)
+            x = torch.add(x, residual)
+            x = self.layer_normalization(x)
+
+            residual = x
+
+            x = self.feed_forward_network(x)
+            x = self.dropout(x)
+            x = torch.add(x, residual)
+            x = self.layer_normalization(x)
+
             return x
         else:
             raise ValueError("Input must be a torch.Tensor.")
