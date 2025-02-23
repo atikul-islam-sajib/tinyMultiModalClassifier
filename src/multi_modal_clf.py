cef60dd2375a9120399ee19641f6dd1e1a55fa08
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-23 08:51:45 +0100

    multi modal classifier class is addeed with fusion - do it later

--- src/multi_modal_clf.py
+++ src/multi_modal_clf.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 
 sys.path.append("./src/")
 
+from utils import config_files
 from ViT import VisionTransformer
 from text_transformer import TextTransformerEncoder
 
@@ -20,7 +21,7 @@ class MultiModalClassifier(nn.Module):
         nheads: int = 8,
         num_encoder_layers: int = 6,
         dim_feedforward: int = 1024,
-        dropout: float = 0.0,
+        dropout: float = 0.1,
         layer_norm_eps: float = 1e-05,
         activation: str = "relu",
     ):
@@ -37,12 +38,70 @@ class MultiModalClassifier(nn.Module):
         self.layer_norm_eps = layer_norm_eps
         self.activation = activation
 
+        self.vision_transformer = VisionTransformer(
+            channels=channels,
+            patch_size=patch_size,
+            image_size=image_size,
+            dimension=dimension,
+            nheads=nheads,
+            num_encoder_layers=num_encoder_layers,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            layer_norm_eps=layer_norm_eps,
+            activation=activation,
+        )
+
+        self.text_transformer = TextTransformerEncoder(
+            dimension=self.dimension,
+            nheads=self.nheads,
+            num_encoder_layers=self.num_encoder_layers,
+            dim_feedforward=self.dim_feedforward,
+            dropout=self.dropout,
+            layer_norm_eps=self.layer_norm_eps,
+            activation=self.activation,
+        )
+
     def forward(self, image: torch.Tensor, text: torch.Tensor):
         if isinstance(image, torch.Tensor) and isinstance(text, torch.Tensor):
-            pass
+            image_features = self.vision_transformer(image)
+            text_features = self.text_transformer(text)
+
+            return {"image_features": image_features, "text_features": text_features}
         else:
             raise ValueError("Both inputs must be torch.Tensor.".capitalize())
 
 
 if __name__ == "__main__":
-    pass
+    image_channels = config_files()["patchEmbeddings"]["channels"]
+    image_size = config_files()["patchEmbeddings"]["image_size"]
+    patch_size = config_files()["patchEmbeddings"]["patch_size"]
+    dimension = config_files()["patchEmbeddings"]["dimension"]
+    nheads = config_files()["transfomerEncoderBlock"]["nheads"]
+    activation = config_files()["transfomerEncoderBlock"]["activation"]
+    dropout = config_files()["transfomerEncoderBlock"]["dropout"]
+    num_encoder_layers = config_files()["transfomerEncoderBlock"]["num_encoder_layers"]
+    dimension_feedforward = config_files()["transfomerEncoderBlock"][
+        "dimension_feedforward"
+    ]
+    layer_norm_eps = float(config_files()["transfomerEncoderBlock"]["layer_norm_eps"])
+
+    number_of_patches = (image_size // patch_size) ** 2
+    number_of_sequences = (image_size // patch_size) ** 2
+
+    images = torch.randn(1, image_channels, image_size, image_size)
+    texts = torch.randint(0, number_of_sequences, (1, number_of_sequences))
+
+    classifier = MultiModalClassifier(
+        channels=image_channels,
+        patch_size=patch_size,
+        image_size=image_size,
+        dimension=dimension,
+        nheads=nheads,
+        num_encoder_layers=num_encoder_layers,
+        dim_feedforward=dimension_feedforward,
+        dropout=dropout,
+        layer_norm_eps=layer_norm_eps,
+        activation=activation,
+    )
+    output = classifier(image=images, text=texts)
+    print(output["image_features"].size(), output["text_features"].size())
