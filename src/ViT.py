7f6cff615729a9c6cbd5157bf3dc901dac5ef78e
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-23 07:29:36 +0100

    ViT prototype is added

---
+++ src/ViT.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import torch
+import argparse
+import torch.nn as nn
+
+sys.path.append("./src/")
+
+
+class VisionTransformer(nn.Module):
+    pass
+
+
+if __name__ == "__main__":
+    pass
