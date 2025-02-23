da16c603b055c9b92eea24311c96bd5818187408
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-02-23 07:46:33 +0100

    ViT is added with CLI

--- src/ViT.py
+++ src/ViT.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 
 sys.path.append("./src/")
 
+from utils import config_files
 from patch_embedding import PatchEmbedding
 from transformer_encoder import TransformerEncoder
 
@@ -63,17 +64,103 @@ class VisionTransformer(nn.Module):
 
 
 if __name__ == "__main__":
-    image = torch.randn((1, 3, 128, 128))
-    vision_transformer = VisionTransformer(
-        channels=3,
-        patch_size=16,
-        image_size=128,
-        dimension=256,
-        nheads=8,
-        num_encoder_layers=6,
-        dim_feedforward=1024,
-        dropout=0.0,
-        layer_norm_eps=1e-05,
-        activation="relu",
+    image_channels = config_files()["patchEmbeddings"]["channels"]
+    image_size = config_files()["patchEmbeddings"]["image_size"]
+    patch_size = config_files()["patchEmbeddings"]["patch_size"]
+    dimension = config_files()["patchEmbeddings"]["dimension"]
+    nheads = config_files()["transfomerEncoderBlock"]["nheads"]
+    activation = config_files()["transfomerEncoderBlock"]["activation"]
+    dropout = config_files()["transfomerEncoderBlock"]["dropout"]
+    num_encoder_layers = config_files()["transfomerEncoderBlock"]["num_encoder_layers"]
+    dimension_feedforward = config_files()["transfomerEncoderBlock"][
+        "dimension_feedforward"
+    ]
+    layer_norm_eps = float(config_files()["transfomerEncoderBlock"]["layer_norm_eps"])
+
+    parser = argparse.ArgumentParser(
+        description="Vision Transformer for multimodal model".title()
     )
-    print(vision_transformer(image).size())
+
+    parser.add_argument(
+        "--image_channels",
+        type=int,
+        default=image_channels,
+        help="Image channels to transform".capitalize(),
+    )
+    parser.add_argument(
+        "--image_size",
+        type=int,
+        default=image_size,
+        help="Image size to transform".capitalize(),
+    )
+    parser.add_argument(
+        "--patch_size",
+        type=int,
+        default=patch_size,
+        help="Patch size for the transformer".capitalize(),
+    )
+    parser.add_argument(
+        "--dimension",
+        type=int,
+        default=dimension,
+        help="Dimension for the transformer".capitalize(),
+    )
+    parser.add_argument(
+        "--nheads",
+        type=int,
+        default=nheads,
+        help="Number of heads for the multi-head attention".capitalize(),
+    )
+    parser.add_argument(
+        "--activation",
+        type=str,
+        default=activation,
+        help="Activation function for the transformer".capitalize(),
+    )
+    parser.add_argument(
+        "--dropout",
+        type=float,
+        default=dropout,
+        help="Dropout probability for the transformer".capitalize(),
+    )
+    parser.add_argument(
+        "--num_encoder_layers",
+        type=int,
+        default=num_encoder_layers,
+        help="Number of layers in the transformer".capitalize(),
+    )
+    parser.add_argument(
+        "--dimension_feedforward",
+        type=int,
+        default=dimension_feedforward,
+        help="Dimension for the feedforward layer".capitalize(),
+    )
+    parser.add_argument(
+        "--layer_norm_eps",
+        type=float,
+        default=layer_norm_eps,
+        help="Layer normalization epsilon".capitalize(),
+    )
+
+    args = parser.parse_args()
+
+    image = torch.randn((1, image_channels, image_size, image_size))
+    num_of_patches = (image_size // patch_size) ** 2
+
+    vision_transformer = VisionTransformer(
+        channels=args.image_channels,
+        patch_size=args.patch_size,
+        image_size=args.image_size,
+        dimension=args.dimension,
+        nheads=args.nheads,
+        num_encoder_layers=args.num_encoder_layers,
+        dim_feedforward=args.dimension_feedforward,
+        dropout=args.dropout,
+        layer_norm_eps=args.layer_norm_eps,
+        activation=args.activation,
+    )
+    assert (vision_transformer(image).size()) == (
+        1,
+        num_of_patches,
+        dimension,
+    ), "ViT is not working properly".capitalize()
