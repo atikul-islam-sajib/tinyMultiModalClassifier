6ff129b2abc755431c0f4310f566db50915b62e6
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2025-02-25 09:59:23 +0100

    helper function abstract is added

---
+++ src/helper.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import argparse
+import torch
+import torch.nn as nn
+
+sys.path.append("./src/")
+
+def load_dataloader():
+    pass
+
+def helper(**kwargs):
+    pass
+
+
+if __name__ == "__main__":
+    pass
